f55c46b6ed2c2f83806754d121c4de523e10bf54
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-11-30 20:34:00 +0000

    first commit: add tests

---
+++ tests/test_monoid.py
@@ -0,0 +1,43 @@
+from categories.monoid import monoid, Monoid
+import random
+import pytest
+from typing import Type, TypeVar, Callable
+import operator
+
+
+T = TypeVar('T')
+
+def randint() -> int:
+    return random.randint(0, 200)
+
+
+def get_random(total: int, random_genrator: Callable[[], T]) -> list[T]:
+    return [random_genrator() for _ in range(total)] 
+
+
+def get_triples(total: int, random_generator: Callable[[], T]) -> list[tuple[T, T, T]]:
+    f = random_generator
+
+    return [(f(), f(), f()) for _ in range(total)]
+
+
+@pytest.mark.parametrize("triple", get_triples(100, randint))
+def test_monoid_associative(
+    # t: Type, 
+    # identity: T, 
+    # op: Callable[[T, T], T], 
+    triple: tuple[T, T, T]
+):
+    
+    M = monoid(int, 0, operator.add)
+
+    assert M.test_associativity(triple) == True
+
+
+@pytest.mark.parametrize("random_value", get_random(200, randint))
+def test_monoid_identity(
+    random_value: T
+):
+    M = monoid(int, 0, operator.add)
+
+    assert M.test_identity(random_value) == True
