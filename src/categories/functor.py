8506300eb73cf179326f451dc1d1f280bfbe88be
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-12-02 15:01:48 +0000

    more flexible functor

--- src/categories/functor.py
+++ src/categories/functor.py
@@ -5,38 +5,41 @@ from copy import deepcopy
 T = TypeVar('T')
 U = TypeVar('U')
 V = TypeVar('V')
+W = TypeVar('W')
 
-F = Callable[[T], T]
-G = Callable[[U], U]
+F = Callable[[T], U]
+G = Callable[[V], W]
 H = Callable[[F], G]
 
-class Functor(Generic[T, U]):
+class Functor(Generic[T, U, V, W]):
 
-    _value: U
+    _value: V
     _functor: functor
 
-    def __init__(self, value: U, _functor: functor):
+    def __init__(self, value: V, _functor: functor):
         self._value = value
         self._functor = _functor
 
     def __str__(self) -> str:
         return f'Functor({self._functor}, {str(self._value)})'
 
-    def fmap(self, f: Callable[[T], T]) -> U:
+    def fmap(self, f: Callable[[T], U]) -> W:
         return self._functor._fmap(f)(self._value)
 
 
-class functor(Generic[T, U]):
+class functor(Generic[T, U, V, W]):
     _t: type[T]
     _u: type[U]
+    _v: type[V]
+    _w: type[W]
     _fmap: H
 
-    def __init__(self, t: type[T], u: type[U], fmap: Callable[[F], G]):
-        self._t = t
-        self._u = u
+    def __init__(self, ftypes: tuple[type[T], type[U]], gtypes: tuple[type[V], type[W]], fmap: Callable[[F], G]):
+        self._t, self._u = ftypes
+        self._v, self._w = gtypes
         self._fmap = fmap
 
-    def __call__(self, v: U) -> Functor[T, U]:
+    def __call__(self, v: V) -> Functor[T, U, V, W]:
         return Functor(v, self)
 
     def __str__(self) -> str:
@@ -46,9 +49,9 @@ class functor(Generic[T, U]):
 if __name__ == '__main__':
     t = list
 
-    def listfmap(f: F) -> Callable[[list[T]], list[T]]:
+    def listfmap(f: Callable[[T], U]) -> Callable[[list[T]], list[U]]:
 
-        def _listfmap(l: list) -> list:
+        def _listfmap(l: list[T]) -> list[U]:
             return list(map(f, l))
 
         return _listfmap
@@ -56,7 +59,7 @@ if __name__ == '__main__':
     def add10(x: int) -> int:
         return x + 10
     
-    Func = functor(int, list[int], listfmap)
+    Func = functor((int, int), (list[int], list[int]), listfmap)
     func = Func([1, 2, 3])
     result = func.fmap(add10)
     print(result)
