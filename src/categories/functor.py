4600cd49b36c15330aa1f0e6b6287e071026b43b
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-12-02 14:54:07 +0000

    fmap working

--- src/categories/functor.py
+++ src/categories/functor.py
@@ -3,52 +3,60 @@ from typing import Generic, TypeVar, Callable, Iterable, Any
 from copy import deepcopy
 
 T = TypeVar('T')
+U = TypeVar('U')
+V = TypeVar('V')
 
-F = Callable[[Any], Any]
-G = Callable[[T], T]
+F = Callable[[T], T]
+G = Callable[[U], U]
 H = Callable[[F], G]
 
-class Functor(Generic[T]):
+class Functor(Generic[T, U]):
 
-    _value: T
+    _value: U
     _functor: functor
 
-    def __init__(self, value: T, _functor: functor):
+    def __init__(self, value: U, _functor: functor):
         self._value = value
         self._functor = _functor
 
     def __str__(self) -> str:
         return f'Functor({self._functor}, {str(self._value)})'
 
-    def fmap(self, f: Callable[[Any], Any]) -> T:
+    def fmap(self, f: Callable[[T], T]) -> U:
         return self._functor._fmap(f)(self._value)
 
 
-class functor(Generic[T]):
-    _type: type[T]
+class functor(Generic[T, U]):
+    _t: type[T]
+    _u: type[U]
     _fmap: H
 
-    def __init__(self, t: type[T], fmap: H):
-        self._type = t
+    def __init__(self, t: type[T], u: type[U], fmap: Callable[[F], G]):
+        self._t = t
+        self._u = u
         self._fmap = fmap
 
-    def __call__(self, v: T) -> Functor[T]:
+    def __call__(self, v: U) -> Functor[T, U]:
         return Functor(v, self)
 
     def __str__(self) -> str:
-        return f'functor({self._type}, {self._fmap})'
+        return f'functor({self._t} -> {self._u}, {self._fmap})'
 
 
 if __name__ == '__main__':
     t = list
 
-    def listfmap(f: F) -> Callable[[list], list]:
+    def listfmap(f: F) -> Callable[[list[T]], list[T]]:
 
         def _listfmap(l: list) -> list:
             return list(map(f, l))
 
         return _listfmap
+
+    def add10(x: int) -> int:
+        return x + 10
     
-    Func = functor(list, listfmap)
+    Func = functor(int, list[int], listfmap)
     func = Func([1, 2, 3])
-    result = func.fmap(lambda x: x+1)
+    result = func.fmap(add10)
+    print(result)
